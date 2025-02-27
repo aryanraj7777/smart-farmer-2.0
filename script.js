e6a33bad303a6411b9eb20074f4cffb277d0e486
Aryan raj <103170668+aryanraj7777@users.noreply.github.com>
2025-02-27 18:27:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+document.getElementById("loginForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    window.location.href = "dashboard.html";
+});
+function searchCrop() {
+    let condition = document.getElementById("searchBar").value;
+    document.getElementById("cropResult").innerText = "Suggested Crop: Wheat";
+}
 
